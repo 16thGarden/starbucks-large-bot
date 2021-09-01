c114ee5fc7dc2624230658e670253886fff2e10b
MiguelLeyesa <jose_miguel_leyesa@dlsu.edu.ph>
2021-09-01 23:59:08 +0800

    Update whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -1,5 +1,5 @@
 module.exports = [
     421569486806843394, // starbucks large#9515
-    140743969188478976, // JPGomdol#4088
+    // 140743969188478976, // 
     843099865071484960, // 420fuckywuckysussybaka69amogus#8232
 ]
